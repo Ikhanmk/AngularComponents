712314780d0f1ff29ffe5da1e7769d90ba5e869a
ikhanmk <ikhanmk@gmail.com>
2022-03-24 11:10:26 +0530

    modules-modules interaction

--- AngComponents/src/app/app.module.ts
+++ AngComponents/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UsersModule } from './users/users.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
